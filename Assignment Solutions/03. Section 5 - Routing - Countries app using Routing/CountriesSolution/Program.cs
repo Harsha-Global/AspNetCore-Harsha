e62136472d6aa81d1bc8009b99c14613d55a79de
Harsha-Global <53711684+Harsha-Global@users.noreply.github.com>
2025-07-12 19:46:49 +0530

    Update Program.cs

--- Assignment Solutions/03. Section 5 - Routing - Countries app using Routing/CountriesSolution/Program.cs
+++ Assignment Solutions/03. Section 5 - Routing - Countries app using Routing/CountriesSolution/Program.cs
@@ -16,57 +16,41 @@ Dictionary<int, string> countries = new Dictionary<int, string>()
 //endpoints
 app.UseEndpoints(endpoints =>
 {
- //when request path is "/countries"
+ //Route 1: when request path is "/countries" (list all countries)
  endpoints.MapGet("/countries", async context =>
  {
-  //read all countries
   foreach (KeyValuePair<int, string> country in countries)
   {
-   //write country details to response
    await context.Response.WriteAsync($"{country.Key}, {country.Value}\n");
   }
  });
 
-
- //When request path is "countries/{countryID}"
+ //Route 2: when request path is "/countries/{countryID}" for IDs 1-100
  endpoints.MapGet("/countries/{countryID:int:range(1,100)}", async context =>
  {
-  //check if "countryID" was not submitted in the request
-  if (context.Request.RouteValues.ContainsKey("countryID") == false)
-  {
-   context.Response.StatusCode = 400;
-   await context.Response.WriteAsync("The CountryID should be between 1 and 100");
-  }
-
-
-  //read countryID from RouteValues (route parameters)
+  //read countryID from RouteValues
   int countryID = Convert.ToInt32(context.Request.RouteValues["countryID"]);
-
-
+  
   //if the countryID exists in the countries dictionary
   if (countries.ContainsKey(countryID))
   {
    string countryName = countries[countryID];
-
-   //write country name to response
    await context.Response.WriteAsync($"{countryName}");
   }
-
-  //if countryID not exists in countries dictionary
+  //if countryID doesn't exist in countries dictionary (but is 1-100 range)
   else
   {
    context.Response.StatusCode = 404;
-   await context.Response.WriteAsync($"[No country]");
+   await context.Response.WriteAsync("[No Country]");
   }
  });
 
- //When request path is "countries/{countryID}"
- endpoints.MapGet("/countries/{countryID:min(101)}", async context =>
+ //Route 3: when request path is "/countries/{countryID}" for IDs greater than 100
+ endpoints.MapGet("/countries/{countryID:int:min(101)}", async context =>
  {
   context.Response.StatusCode = 400;
-  await context.Response.WriteAsync("The CountryID should be between 1 and 100 - min");
+  await context.Response.WriteAsync("The CountryID should be between 1 and 100");
  });
-
 });
 
 //Default middleware
