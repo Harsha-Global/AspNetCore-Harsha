dea1b52ed87bd9742bc31b93656584def851dd47
fyf <57135698+1250890838@users.noreply.github.com>
2025-12-27 10:46:19 +0800

    Make OrderDate and InvoicePrice nullable
    Changed OrderDate and InvoicePrice properties to nullable types.

--- Assignment Solutions/05. Section 7 - Model Binding and Validations - Orders app/OrderSolution/Models/Order.cs
+++ Assignment Solutions/05. Section 7 - Model Binding and Validations - Orders app/OrderSolution/Models/Order.cs
@@ -11,13 +11,13 @@ namespace OrderSolution.Models
   [Required(ErrorMessage = "{0} can't be blank")]
   [Display(Name = "Order Date")]
   [MinimumDateValidator("2000-01-01", ErrorMessage = "Order Date should be greater than or equal to {0}")] //custom validator
-  public DateTime OrderDate { get; set; }
+  public DateTime? OrderDate { get; set; }
 
   [Required(ErrorMessage = "{0} can't be blank")]
   [Display(Name = "Invoice Price")]
   [InvoicePriceValidator] //custom validator
   [Range(1, double.MaxValue, ErrorMessage = "{0} should be between a valid number")]
-  public double InvoicePrice { get; set; }
+  public double? InvoicePrice { get; set; }
 
   [ProductsListValidator] //custom validator
   public List<Product> Products { get; set; } = new List<Product>();
